e86988151f8569bee5eb8c857934a03252aa1a4d
pengfei Yao <import.pfy@gmail.com>
2021-01-27 20:41:32 -0500

    adding user view

---
+++ shoppingmall-user-management/src/main/java/com/py/shoppingmall/ShoppingmallUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.py.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingmallUserManagementApplication.class, args);
+	}
+
+}
